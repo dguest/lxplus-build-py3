073022cfb92b476d8372c9994996765df15e45ca
Dan Guest <dguest@cern.ch>
2018-07-22 17:39:48 +0200

    Add centos7 setup

--- download-and-build.sh
+++ download-and-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # version settings
-VERSION=3.7.0
+VERSION=3.6.6
 SOURCE_DIR=Python-${VERSION}
 TARBALL=${SOURCE_DIR}.tgz
 
@@ -16,6 +16,6 @@ fi
 
 # build
 cd ${SOURCE_DIR}
-./configure --prefix=${HOME}/public/${SOURCE_DIR} --with-openssl=/cvmfs/cms.cern.ch/slc6_amd64_gcc620/external/openssl/1.0.2d
+./configure --prefix=${HOME}/public/${SOURCE_DIR}
 make -j 4
 make install
